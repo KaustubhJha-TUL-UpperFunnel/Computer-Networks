0a3f3c7b4da2e2dd58fae67d9a9d3c96103d995e
kimj0588 <47002247+kimj0588@users.noreply.github.com>
2019-10-14 19:18:40 +0530

    Computer Networks Project start

---
+++ Project/client.py
@@ -0,0 +1,79 @@
+# from socket import *
+# serverPort = 12005
+# serverSocket = socket(AF_INET,SOCK_STREAM)
+# serverSocket.bind(('127.0.0.1',serverPort))
+# serverSocket.listen(1)
+# print 'The server is ready to receive'
+# while 1:
+# 		connectionSocket, addr = serverSocket.accept()
+# 		message = connectionSocket.recv(1024*5)
+# 		print message
+# 		temp = list(message.split())
+# 		try:
+# 			name=temp[1][1:]
+# 			f = open(name)
+# 			contents = f.read()
+# 			connectionSocket.send('HTTP/1.1 200 OK\r\n')
+# 			connectionSocket.send('Content-Type: text/html\r\n')
+# 			connectionSocket.send('\r\n')
+# 			connectionSocket.send(contents)
+# 			connectionSocket.close()
+# 		except:
+# 			f = open("test.html")
+# 			contents = f.read()
+# 			connectionSocket.send('HTTP/1.1 404 Not Found\r\n')
+# 			connectionSocket.send('Content-Type: text/html\r\n')
+# 			connectionSocket.send('\r\n')
+# 			connectionSocket.send(contents)
+# 			connectionSocket.close()
+
+import tkinter as tk
+from functools import partial
+import socket
+
+
+serverSocket = 0
+clientSocket = 0
+
+
+def action(text):
+	var = text.get("1.0", "end-1c")
+	# print(var)
+	global clientSocket
+	var = var.encode()
+	clientSocket.sendto(var,('localhost', 12000))
+	# message, clientAddress = serverSocket.recvfrom(2048)
+	# print(message)
+
+
+# def server(text):
+# 	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 	serverSocket.bind(('', 12000))
+# 	# while(True):
+# 		# message, clientAddress = serverSocket.recvfrom(2048)
+# 	# text.set(message)
+# 		# print(message)
+
+def client():
+	serverName = 'localhost'
+	serverPort = 12000
+	global clientSocket
+	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+root = tk.Tk()
+root.title("Working of Checksum")
+root.geometry("400x400")
+
+
+text = tk.Text(root, bg = "grey", bd = 3)
+action_arg = partial(action, text)
+c_server = tk.Button(root, text = "Create Server", command = lambda: server(text))
+c_client = tk.Button(root, text = "Create Client", command = client)
+c_sendMessage = tk.Button(root, text = "Send Message", command = lambda: action(text))
+
+
+c_server.place(relwidth = 0.2, relheight = 0.1, relx = 0.1, rely = 0.1)
+c_client.place(relwidth = 0.2, relheight = 0.1, relx = 0.7, rely = 0.1)
+text.place(relwidth = 0.5, relheight = 0.3, relx = 0.25, rely = 0.25)
+c_sendMessage.place(relx = 0.375, rely = 0.6)
+root.mainloop()
