e970eb97c4520570ae9cdf1dbef7a6986ecfb4a2
Tushar Raj <43772524+tushar-1728@users.noreply.github.com>
2019-10-22 18:36:11 +0530

    Add files via upload

--- Project/client.py
+++ Project/client.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from functools import partial
+# from functools import partial
 import socket
 
 
@@ -7,29 +7,19 @@ serverSocket = 0
 clientSocket = 0
 
 
-def action(text):
-	var = text.get("1.0", "end-1c")
-	# print(var)
+def send_message(text):
 	global clientSocket
+	var = text.get("1.0", "end-1c")
+	print(type(var))
 	var = var.encode()
 	clientSocket.sendto(var,('localhost', 12000))
-	# message, clientAddress = serverSocket.recvfrom(2048)
-	# print(message)
+	print(var)
 
-
-# def server(text):
-# 	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# 	serverSocket.bind(('', 12000))
-# 	# while(True):
-# 		# message, clientAddress = serverSocket.recvfrom(2048)
-# 	# text.set(message)
-# 		# print(message)
 
 def client():
-	serverName = 'localhost'
-	serverPort = 12000
 	global clientSocket
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	print("client created")
 
 root = tk.Tk()
 root.title("Working of Checksum")
@@ -37,14 +27,13 @@ root.geometry("400x400")
 
 
 text = tk.Text(root, bg = "grey", bd = 3)
-action_arg = partial(action, text)
-c_server = tk.Button(root, text = "Create Server", command = lambda: server(text))
-c_client = tk.Button(root, text = "Create Client", command = client)
-c_sendMessage = tk.Button(root, text = "Send Message", command = lambda: action(text))
-
-
-c_server.place(relwidth = 0.2, relheight = 0.1, relx = 0.1, rely = 0.1)
-c_client.place(relwidth = 0.2, relheight = 0.1, relx = 0.7, rely = 0.1)
 text.place(relwidth = 0.5, relheight = 0.3, relx = 0.25, rely = 0.25)
+
+c_client = tk.Button(root, text = "Create Client", command = client)
+c_client.place(relwidth = 0.2, relheight = 0.1, relx = 0.7, rely = 0.1)
+
+c_sendMessage = tk.Button(root, text = "Send Message", command = lambda: send_message(text))
 c_sendMessage.place(relx = 0.375, rely = 0.6)
+
+
 root.mainloop()
--- Project/server.py
+++ Project/server.py
@@ -1,36 +1,27 @@
 import tkinter as tk
-from functools import partial
+import threading
 import socket
 
 
 global serverSocket
 global clientSocket
 
-
-def action(text):
-	var = text.get("1.0", "end-1c")
-	print(var)
-	clientSocket.sendto(var,('localhost', 12000))
+def recv_message(text, serverSocket):
 	message, clientAddress = serverSocket.recvfrom(2048)
+	message = str(message.decode())
+	text.insert("1.0", message+"\n")
 	print(message)
+	if(message != "bye"):
+		recv_message(text, serverSocket)
 
 
 def server(text):
 	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	serverSocket.bind(('', 12000))
-	print("server created")
-	flag = True
-	while (flag):
-		message, clientAddress = serverSocket.recvfrom(2048)
-		message = message.decode()
-		print(message)
-		if(message == "bye"):
-			flag = False
+	serverSocket.bind(('localhost', 12000))
+	t1 = threading.Thread(target = recv_message, args = [text, serverSocket])
+	t1.start()
+		
 
-def client():
-	serverName = 'localhost'
-	serverPort = 12000
-	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 root = tk.Tk()
 root.title("Working of Checksum")
@@ -38,14 +29,12 @@ root.geometry("400x400")
 
 
 text = tk.Text(root, bg = "grey", bd = 3)
-action_arg = partial(action, text)
+# action_arg = partial(action, text)
 c_server = tk.Button(root, text = "Create Server", command = lambda: server(text))
-c_client = tk.Button(root, text = "Create Client", command = client)
 c_sendMessage = tk.Button(root, text = "Send Message", command = lambda: action(text))
 
 
 c_server.place(relwidth = 0.2, relheight = 0.1, relx = 0.1, rely = 0.1)
-c_client.place(relwidth = 0.2, relheight = 0.1, relx = 0.7, rely = 0.1)
 text.place(relwidth = 0.5, relheight = 0.3, relx = 0.25, rely = 0.25)
 c_sendMessage.place(relx = 0.375, rely = 0.6)
 root.mainloop()
